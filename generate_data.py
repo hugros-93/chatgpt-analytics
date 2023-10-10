8398efeb52a5ed4c666d09c92d820c409fa54768
Hugo De Oliveira <80337112+hugros-93@users.noreply.github.com>
2023-10-11 00:01:29 +0200

    add generate data

--- generate_data.py
+++ generate_data.py
@@ -2,18 +2,23 @@ import pandas as pd
 import random
 from datetime import datetime, timedelta
 
-category_list = ['blue', 'red', 'green']
+def generate_input_data():
 
-data = []
-for category in category_list:
-    date = datetime(2020,1,1)
-    value = random.randint(0,1000)
-    while date < datetime.now():
-        data.append(
-            [category, date, value]
-        )
-        date = date + timedelta(days = 1)
-        value = value + random.randint(-10,10)
+    category_list = ['blue', 'red', 'green']
 
-data = pd.DataFrame(data, columns=['category', 'date', 'value'])
-data.to_csv('input_data/input_data.csv', index=False)
+    data = []
+    for category in category_list:
+        date = datetime(2020,1,1)
+        value = random.randint(0,1000)
+        while date < datetime.now():
+            data.append(
+                [category, date, value]
+            )
+            date = date + timedelta(days = 1)
+            value = value + random.randint(-10,10)
+
+    data = pd.DataFrame(data, columns=['category', 'date', 'value'])
+    data.to_csv('input_data/input_data.csv', index=False)
+
+if __name__ == "__main__":
+    generate_input_data()
