2c314628323434bc868d2cef1186813b6044ce77
Hugo De Oliveira <80337112+hugros-93@users.noreply.github.com>
2023-10-10 23:54:23 +0200

    script to generate input data

---
+++ generate_data.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import random
+from datetime import datetime, timedelta
+
+category_list = ['blue', 'red', 'green']
+
+data = []
+for category in category_list:
+    date = datetime(2020,1,1)
+    value = random.randint(0,1000)
+    while date < datetime.now():
+        data.append(
+            [category, date, value]
+        )
+        date = date + timedelta(days = 1)
+        value = value + random.randint(-10,10)
+
+data = pd.DataFrame(data, columns=['category', 'date', 'value'])
+data.to_csv('input_data/input_data.csv', index=False)
